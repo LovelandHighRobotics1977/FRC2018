05a55bc724cd492734c6a9ba248acaa7a1597bb6
dmgowins20 <dmgowins20@LO0145W06076535.thompsonschools.org>
2018-02-20 17:22:50 -0700

    Added simple autonomous

--- FRC 2018/src/org/usfirst/frc/team1977/robot/commands/AutonomousDefault.java
+++ FRC 2018/src/org/usfirst/frc/team1977/robot/commands/AutonomousDefault.java
@@ -4,9 +4,16 @@ import org.usfirst.frc.team1977.robot.commands.pneumatic.PneumaticOn;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+import org.usfirst.frc.team1977.robot.commands.drive.DriveStop;
+import org.usfirst.frc.team1977.robot.commands.drive.DriveTime;
+import org.usfirst.frc.team1977.robot.commands.drive.TurnTime;
+
+
 public class AutonomousDefault extends CommandGroup {
 	public AutonomousDefault() {
 		addSequential(new PneumaticOn());
-		
+		addSequential(new DriveTime(0, 1, 2000));
+		addSequential(new TurnTime(500, 1));
+		addSequential(new DriveTime(0, 1, 1000));
 	}
 }
